fed3a781633013fae6702ed14997651e8f91b4d0
javier <javier-dc01>
2021-06-14 02:09:39 +0000

    Working on boot device safety measure

--- vulcan.py
+++ vulcan.py
@@ -2,9 +2,9 @@
 
 from subprocess import check_output
 from sys import platform
-
+import pprint
 # Step Group 1: Safety checks
-print("Performing OS and safety checks...\n")
+print("Performing OS and safety checks...")
 
 # Step1.1: Check for linux environment
 #TODO: Remove darwin once finished prototyping
@@ -13,14 +13,24 @@ accepted_os = ["linux", "darwin"]
 if platform not in accepted_os:
     print("Your OS is currently not supported. Exiting...")
     exit(99)
+else:
+    print("OS Check: PASS (%s)" % (platform))
 
 # Step1.2: Check for boot device, and ban it from ever being touched
 #Only works for linux for now
+boot_devices = ["/boot", "/System/Volumes/Data"]
 df = check_output(['df']).decode()
 dflines = df.split('\n')
-for line in dflines:
-        if '/boot' in line:
-                print(line.split(' ')[0])
 
+for line in dflines:
+    for item in boot_devices:
+        if item in line:
+          print(line)
+        else:
+          pass
+"""
+pprint.pprint(dflines)
+print(type(dflines))
+"""
 
 
